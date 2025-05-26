8cd32b542de71e139c834bb1da4485cbfa505d7d
Obinna Uche <obinnauche01@outlook.com>
2025-05-26 16:52:38 +0100

    Added match case to allow different operations & converted input to string

--- calculator.py
+++ calculator.py
@@ -1,10 +1,30 @@
-x = input("Enter number:")
+x = 0
 
-y = input("Enter numeber:")
+y = 0
 
-def add(x, y):
-    return x + y
+
+first_input = input("Enter number:")
+
+second_input = input("Enter numeber:")
+
+x = int(first_input)
+y = int(second_input)
 
 command = input("type your command for add, sub, mul, div:")
-if command == "add":
-    print(add(x,y))
+
+match command:
+    case 'add':
+          print(x + y)
+    case 'sub':
+          print(x - y)
+    case 'mul':
+          print(x * y)
+    case 'div':
+          print(x / y)
+    case _ :
+          print("Command not found")
+            
+            
+          
+
+
